9808263964ffacf91bb3a9fe0397947c330d87c2
Dorian Terbah <dorian.terb@gmail.com>
2023-02-06 15:10:42 +0100

    First commit / Setup

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import { BootstrapVue3 } from 'bootstrap-vue-3'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+createApp(App).use(store)
+    .use(router)
+    .use(BootstrapVue3)
+    .mount('#app')
